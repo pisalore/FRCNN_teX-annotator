d4e84037fc6201df51f1ff08dc6b4477e3fb2789
pisalore <lorenzo.pisaneschi@gmail.com>
2020-01-18 17:42:59 +0100

    TEX reader updated (\abstract and \title)

--- venv/tex_reader.py
+++ venv/tex_reader.py
@@ -1,29 +1,31 @@
-fp = open('samplebody-conf.tex', 'rb')
+fp = open('aaai.tex', 'rb')
+title = '\\title'
+abstract = '\\begin{abstract}'
 section = 'section'
 num_sections = 0
 figure = '\\begin{figure}'
 num_figures = 0
 list = '\\begin{itemize}'
-num_list = 0
+num_lists = 0
 table = '\\begin{table}'
 num_tables = 0
 
 for line in fp:
     l = str(line)
-    if l.count(section):
+    if l.count(section) or l.count(title) or l.count(abstract):
         num_sections += 1
         print('Title num: ', num_sections, '   ', l)
     if l.count(figure):
         num_figures += 1
         print('Figure num: ', num_figures, '   ', l)
     if l.count(list):
-        num_list += 1
-        print('List num: ', num_list, '   ', l)
+        num_lists += 1
+        print('List num: ', num_lists, '   ', l)
     if l.count(table):
         num_tables += 1
-        print('Figure num: ', num_table, '   ', l)
+        print('Figure num: ', num_tables, '   ', l)
 
 print ('Sections: ', num_sections)
 print ('Figures: ', num_figures)
-print ('List: ', num_list)
+print ('List: ', num_lists)
 print ('Tables: ', num_tables)
