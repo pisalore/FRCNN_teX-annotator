d10403e0759f3cd311de804726cb640684869496
pisalore <lorenzo.pisaneschi@gmail.com>
2020-01-20 16:06:32 +0100

    Created list which cntains all figures and theirs captions

--- venv/tex_parser.py
+++ venv/tex_parser.py
@@ -1,53 +1,62 @@
 def find_tex_istances(path):
     print('Opening 2001.05970.tex file...')
     fp = open(path, 'rb')
-# TITLES                                    category: 1
+    # TITLES                                    category: 1
     title = '\\title'
     abstract = '\\begin{abstract}'
     section = 'section'
     num_sections = 0
     all_titles = []
 
-#FIGURES                                    category: 2
+    # FIGURES                                    category: 2
     figure = '\\begin{figure}'
     num_figures = 0
     sub_figure = '\\begin{subfigure}'
     num_sub_figure = 0
+    is_figure = False
+    figure_counter = 0
     is_subfigure = False
-    end = '\\end{figure}'
+    caption = '\caption{'
+    end_figure = '\\end{figure}'
+    all_figures = []
 
-#LISTS                                      category: 3
+    # LISTS                                      category: 3
     list = '\\begin{itemize}'
     num_lists = 0
 
-#TABLES                                     category: 4
+    # TABLES                                     category: 4
     table = '\\begin{table}'
     num_tables = 0
-    
+
     for line in fp:
         string_line = str(line)
+        # 1 category 1: titles
         if string_line.count(section) or string_line.count(title) or string_line.count(abstract):
             num_sections += 1
             # CHECK FOR COURSIVE TEXT
             if ('textit' in string_line):
-                for i in range (string_line.count('textit')):
+                for i in range(string_line.count('textit')):
                     replace_value = string_line.split('textit{')[i + 1].split('}')[0]
                     string_line = string_line.replace('\\textit{' + replace_value + '}', replace_value)
 
             # CHECK FOR BOLD TEXT
             if ('textbf' in string_line):
-                for i in range (string_line.count('textbf')):
+                for i in range(string_line.count('textbf')):
                     replace_value = string_line.split('textbf{')[i + 1].split('}')[0]
                     string_line = string_line.replace('\\textbf{' + replace_value + '}', replace_value)
 
             string_line = string_line.replace('\\', '').split('{')[1].split('}')[0]
-            #remove double spaces
+            # remove double spaces
             string_line = string_line.replace('  ', ' ')
             title_to_add = [1, string_line]
             all_titles.append(title_to_add)
             print('Title num:   ', num_sections, '   ', string_line)
         elif string_line.count(figure):
+            # 2 category 2: figures
+            is_figure = True
             num_figures += 1
+            # figure_to_add = [2]
+            # all_figures.append(figure_to_add)
             print('Figure num:  ', num_figures, '   ', string_line)
         elif string_line.count(sub_figure):
             is_subfigure = True
@@ -60,10 +69,16 @@ def find_tex_istances(path):
             num_tables += 1
             print('Table num:   ', num_tables, '   ', string_line)
 
-#CHECK IF I'M IN A SUB-FIGURES LIST
-        if (string_line.count(end) and is_subfigure):
+        # CHECK IF I'M IN A SUB-FIGURES LIST
+        if (string_line.count(end_figure) and is_subfigure):
             num_figures -= 1
             is_subfigure = False
+
+        # SAVE FIGURE CAPTION IN ORDER TO HAVE A UNIVOCAL CORRESPONDENCE
+        if (string_line.count(caption) and is_figure):
+            is_figure = False
+            figure_to_add = [2, string_line]
+            all_figures.append(figure_to_add)
 
     print('Titles:    ', num_sections)
     print('Figures:     ', num_figures)
@@ -71,8 +86,8 @@ def find_tex_istances(path):
     print('List:        ', num_lists)
     print('Tables:      ', num_tables)
     print(all_titles)
+    print(all_figures)
 
-path = '2001.05970.tex'   #path to .tex file
+
+path = '2001.05970.tex'  # path to .tex file
 find_tex_istances(path)
-
-
