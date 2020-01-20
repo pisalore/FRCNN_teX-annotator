1bcee1c4028263d9a7ab698d3f3e883b105bda1e
pisalore <lorenzo.pisaneschi@gmail.com>
2020-01-20 18:09:56 +0100

    Started find lists in tex file

--- venv/tex_parser.py
+++ venv/tex_parser.py
@@ -43,7 +43,15 @@ def find_tex_istances(path):
 
     # LISTS                                      category: 3
     list = '\\begin{itemize}'
+    end_list = '\\end{itemize}'
+    enumerated_list = '\\begin{enumerate}'
+    end_enumerated_list = '\end{enumerate}'
+    item = '\\item'
     num_lists = 0
+    is_list = False
+    list_counter = 0
+    item_counter = 0
+    all_lists = []
 
     # TABLES                                     category: 4
     table = '\\begin{table}'
@@ -72,14 +80,13 @@ def find_tex_istances(path):
             # 2 category 2: figures
             is_figure = True
             num_figures += 1
-            # figure_to_add = [2]
-            # all_figures.append(figure_to_add)
             print('Figure num:  ', num_figures, '   ', string_line)
         elif string_line.count(sub_figure):
             is_subfigure = True
             num_sub_figure += 1
             print('Subfigure num:   ', num_sub_figure, '   ', string_line)
-        elif string_line.count(list):
+        elif string_line.count(list) or string_line.count(enumerated_list):
+            is_list = True
             num_lists += 1
             print('List num:    ', num_lists, '   ', string_line)
         elif string_line.count(table):
@@ -110,6 +117,24 @@ def find_tex_istances(path):
             all_figures.append(figure_to_add)
             figures_counter += 1
 
+        if string_line.count(item) and is_list:
+            if ('textit' in string_line):
+                string_line = refactor_coursive_text(string_line)
+
+            # CHECK FOR BOLD TEXT
+            if ('textbf' in string_line):
+                string_line = refactor_bold_text(string_line)
+
+            item_to_add = [3, list_counter, item_counter, string_line]
+            all_lists.append(item_to_add)
+            item_counter += 1
+
+        if string_line.count(end_enumerated_list or end_list):
+            is_list = False
+            item_counter = 0
+
+
+
     print('Titles:    ', num_sections)
     print('Figures:     ', num_figures)
     print('Subfigures:  ', num_sub_figure)
@@ -117,7 +142,8 @@ def find_tex_istances(path):
     print('Tables:      ', num_tables)
     print(all_titles)
     print(all_figures)
+    print(all_lists)
 
 
-path = '2001.05994.tex'  # path to .tex file
+path = '2001.05970.tex'  # path to .tex file
 find_tex_istances(path)
