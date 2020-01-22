a9498dcdd728152bc68f654e4c4d14f50d0e48c9
pisalore <lorenzo.pisaneschi@gmail.com>
2020-01-22 17:29:34 +0100

    First lists refacotr in tex parser function

--- venv/tex_parser.py
+++ venv/tex_parser.py
@@ -35,7 +35,6 @@ def find_tex_istances(path):
     sub_figure = '\\begin{subfigure}'
     num_sub_figure = 0
     is_figure = False
-    figures_counter = 0
     is_subfigure = False
     caption = '\caption{'
     end_figure = '\\end{figure}'
@@ -49,7 +48,6 @@ def find_tex_istances(path):
     item = '\\item'
     num_lists = 0
     is_list = False
-    list_counter = 0
     item_counter = 0
     all_lists = []
 
@@ -73,7 +71,7 @@ def find_tex_istances(path):
             string_line = string_line.replace('\\', '').split('{')[1].split('}')[0]
             # remove double spaces
             string_line = string_line.replace('  ', ' ')
-            title_to_add = [1, string_line]
+            title_to_add = [1, num_sections, string_line]
             all_titles.append(title_to_add)
             print('Title num:   ', num_sections, '   ', string_line)
         elif string_line.count(figure):
@@ -86,6 +84,7 @@ def find_tex_istances(path):
             num_sub_figure += 1
             print('Subfigure num:   ', num_sub_figure, '   ', string_line)
         elif string_line.count(list) or string_line.count(enumerated_list):
+            # 3 category 3: lists
             is_list = True
             num_lists += 1
             print('List num:    ', num_lists, '   ', string_line)
@@ -113,9 +112,8 @@ def find_tex_istances(path):
             # remove double spaces
             string_line = string_line.replace('  ', ' ')
 
-            figure_to_add = [2, figures_counter, string_line]
+            figure_to_add = [2, num_figures, string_line]
             all_figures.append(figure_to_add)
-            figures_counter += 1
 
         if string_line.count(item) and is_list:
             if ('textit' in string_line):
@@ -125,7 +123,9 @@ def find_tex_istances(path):
             if ('textbf' in string_line):
                 string_line = refactor_bold_text(string_line)
 
-            item_to_add = [3, list_counter, item_counter, string_line]
+            string_line = string_line.split('\\item')[1]
+            string_line = string_line.replace('\\n', '').replace('\\', '')
+            item_to_add = [3, num_lists, item_counter, string_line]
             all_lists.append(item_to_add)
             item_counter += 1
 
@@ -145,5 +145,5 @@ def find_tex_istances(path):
     print(all_lists)
 
 
-path = '2001.05970.tex'  # path to .tex file
+path = '2001.05994.tex'  # path to .tex file
 find_tex_istances(path)
