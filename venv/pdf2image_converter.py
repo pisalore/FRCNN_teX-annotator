83dd5e584aa03955f9995a6d833360f918b36bde
pisalore <lorenzo.pisaneschi@gmail.com>
2020-01-25 16:04:55 +0100

    pdf2image converter modified: dpi = 72; it means that pdf and relative images have the same resolution, so bbox properties make sense

--- venv/pdf2image_converter.py
+++ venv/pdf2image_converter.py
@@ -7,12 +7,12 @@ filename = 'pdf_files/2001.05970.pdf'
 reader = PdfFileReader(open('pdf_files/2001.05970.pdf',  mode="rb"))
 page_number = reader.getNumPages()
 with tempfile.TemporaryDirectory() as path:
-    images_from_path = convert_from_path(filename, output_folder=path, last_page=page_number, first_page=0)
+    images_from_path = convert_from_path(filename, dpi=72, output_folder=path, last_page=page_number, first_page=0)
 
-save_dir = 'jpeg_files'
+save_dir = 'png_files'
 
 i = 0
 for page in images_from_path:
-    base_filename = os.path.splitext(os.path.basename(filename))[0] + str(i) +'.jpg'
-    page.save(os.path.join(save_dir, base_filename), 'JPEG')
+    base_filename = os.path.splitext(os.path.basename(filename))[0] + '_' + str(i) +'.png'
+    page.save(os.path.join(save_dir, base_filename), 'PNG')
     i += 1
