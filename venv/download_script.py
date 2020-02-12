e4a753511f70b833e5ac0c4219ef676e482b6fc5
pisalore <lorenzo.pisaneschi@gmail.com>
2020-02-12 18:49:32 +0100

    Now it's possibile to download pdf file from arxiv with urllib

--- venv/download_script.py
+++ venv/download_script.py
@@ -2,18 +2,22 @@
 import urllib.request
 
 pdf_files_path = 'pdf_files/'
+test = 'test_dw_pdf/'
 tex_files_path = 'tex_files/'
 
 pdf_download_url = 'https://arxiv.org/pdf/'
 source_download_url = 'https://arxiv.org/e-print/'
 year = 10
-month = 1
-file_counter = 0
+month = 12
+file_counter = 6044
 file_identifier = ''
 
 while year <=20:
+    file_counter += 1
     if len(str(month)) == 1:
-        file_identifier = '0' + str(month) + str(year)
+        file_identifier =  str(year) + '0' +str(month)
+    else:
+        file_identifier = str(year) + str(month)
     file_counter_digits = len(str(file_counter))
     if file_counter_digits == 1:
         file_identifier += '.000' + str(file_counter)
@@ -22,10 +26,19 @@ while year <=20:
     elif file_counter_digits == 3:
         file_identifier += '.0' + str(file_counter)
     elif file_counter_digits == 4:
-        file_identifier += '.'+str(file_counter)
+        file_identifier += '.' + str(file_counter)
+
+    print('Downloading ' + file_identifier + '.pdf ...')
+
+    try:
+        urllib.request.urlretrieve(pdf_download_url + file_identifier, test + file_identifier +".pdf")
+    except:
+        file_counter = 0
+        if month == 12:
+            month = 1
+            year += 1
+        else:
+            month += 1
 
 
-
-
-
-    # urllib.request.urlretrieve("https://arxiv.org/pdf/1001.0003", pdf_files_path + "file.pdf")
+# 1001.5471
