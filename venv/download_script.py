462381927a8b57957a970700bc2d89a804885766
pisalore <lorenzo.pisaneschi@gmail.com>
2020-02-12 22:13:10 +0100

    Download script updated

--- venv/download_script.py
+++ venv/download_script.py
@@ -1,5 +1,7 @@
 #tes per scaricare files da arXIv
 import urllib.request
+import os
+import tarfile
 
 pdf_files_path = 'pdf_files/'
 test = 'test_dw_pdf/'
@@ -8,8 +10,8 @@ tex_files_path = 'tex_files/'
 pdf_download_url = 'https://arxiv.org/pdf/'
 source_download_url = 'https://arxiv.org/e-print/'
 year = 10
-month = 12
-file_counter = 6044
+month = 2
+file_counter = 1
 file_identifier = ''
 
 while year <=20:
@@ -28,17 +30,30 @@ while year <=20:
     elif file_counter_digits == 4:
         file_identifier += '.' + str(file_counter)
 
-    print('Downloading ' + file_identifier + '.pdf ...')
-
+    print('Downloading ' + file_identifier + '.pdf and its source files from arXiv...')
+    downloaded_pdf_file_path = test + file_identifier +".pdf"
+    downloaded_tar_file_path = test + file_identifier + ".tar"
     try:
-        urllib.request.urlretrieve(pdf_download_url + file_identifier, test + file_identifier +".pdf")
+        urllib.request.urlretrieve(pdf_download_url + file_identifier, downloaded_pdf_file_path)
+        if os.path.exists(downloaded_pdf_file_path):
+            urllib.request.urlretrieve(source_download_url + file_identifier, downloaded_tar_file_path)
+            extract_tar_dir_path = test + file_identifier + '_tex_files'
+            os.mkdir(extract_tar_dir_path)
+            tar = tarfile.open(downloaded_tar_file_path, mode='r:')
+            tar.extractall(path=extract_tar_dir_path)
+            tar.close()
+
+        print('Files succesfully downloaded. \n')
     except:
+        print('PDF or Source files for ' + file_identifier + 'not found. Download the next file.')
         file_counter = 0
         if month == 12:
             month = 1
             year += 1
         else:
             month += 1
+        if os.path.exists(downloaded_pdf_file_path):
+            os.remove(downloaded_pdf_file_path)
 
 
-# 1001.5471
+# 2002.4425
