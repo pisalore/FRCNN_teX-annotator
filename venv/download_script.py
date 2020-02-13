7c2f2f0279a8e5b8ad850fdf8a75de0b4d08f798
pisalore <lorenzo.pisaneschi@gmail.com>
2020-02-13 18:38:26 +0100

    Added check_next_paper function: in case of 404, it will be check de presence of the paper next to the one missing; if this paper misses, the procedure will continue to the next month.

--- venv/download_script.py
+++ venv/download_script.py
@@ -4,6 +4,16 @@ import urllib.error
 import os
 import tarfile
 
+def check_next_paper(file_identifier):
+    p1 = file_identifier.split('.')[0]
+    p2 = file_identifier.split('.')[1]
+    p2 = int(p2) + 1
+    try:
+        urllib.request.urlopen('https://arxiv.org/pdf/' + str(p1) + '.' + str(p2))
+    except:
+        return False
+    return True
+
 pdf_files_path = 'pdf_files/'
 test = 'test_dw_pdf/'
 tex_files_path = 'tex_files/'
@@ -11,7 +21,7 @@ tex_files_path = 'tex_files/'
 pdf_download_url = 'https://arxiv.org/pdf/'
 source_download_url = 'https://arxiv.org/e-print/'
 year = 20
-month = 2
+month = 1
 file_counter = 0
 file_identifier = ''
 
@@ -28,8 +38,9 @@ while year <=20:
         file_identifier += '.00' + str(file_counter)
     elif file_counter_digits == 3:
         file_identifier += '.0' + str(file_counter)
-    elif file_counter_digits == 4:
+    elif file_counter_digits == 4 or file_counter_digits == 5:
         file_identifier += '.' + str(file_counter)
+
 
     print('Downloading ' + file_identifier + '.pdf and its source files from arXiv...')
     downloaded_pdf_file_path = test + file_identifier +".pdf"
@@ -43,18 +54,21 @@ while year <=20:
             downloaded_source_file = tarfile.open(downloaded_source_file_path)
             downloaded_source_file.extractall(path=extract_tar_dir_path)
             downloaded_source_file.close()
+            if os.path.exists(downloaded_source_file_path):
+                os.remove(downloaded_source_file_path)
             print('Files succesfully downloaded. \n')
     except urllib.error.HTTPError:
         print('404: Not Foud. The requested paper does not exist.')
         if os.path.exists(downloaded_pdf_file_path):
             os.remove(downloaded_pdf_file_path)
-        print('PDF or Source files for ' + file_identifier + 'not found. Download the next file.\n')
+        print('PDF or Source files for ' + file_identifier + ' not found. Download the next file.\n')
         file_counter = 0
-        if month == 12:
-            month = 1
-            year += 1
-        else:
-            month += 1
+        if not check_next_paper(file_identifier):
+            if month == 12:
+                month = 1
+                year += 1
+            else:
+                month += 1
         if os.path.exists(downloaded_pdf_file_path):
             os.remove(downloaded_pdf_file_path)
     except tarfile.ReadError:
@@ -65,7 +79,4 @@ while year <=20:
             os.rmdir(extract_tar_dir_path)
         if os.path.exists(downloaded_pdf_file_path):
             os.remove(downloaded_pdf_file_path)
-
-
-
 # 2002.4425
