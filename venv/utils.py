9cca18ca8566bcda3362da6a3496515ccf86697d
pisalore <lorenzo.pisaneschi@gmail.com>
2020-02-20 22:10:56 +0100

    Minor fix

--- venv/utils.py
+++ venv/utils.py
@@ -3,11 +3,11 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--year', default='20', help=' Choose the year from which you want to start'
-                                                       ' downloading papers from arXIv.')
+                                                       ' downloading papers from arXIv. Default 20.')
     parser.add_argument('--month', default='1', help=' Choose the month, once you have choseh the year, from which you want to start'
-                                                     ' downloading papers from arXIv.')
+                                                     ' downloading papers from arXIv. Default 1(janaury)')
     parser.add_argument('--counter', default='0', help='Choose the starting file counter. With 0 you will download all files'
-                                                       'form the year and the month specified.')
+                                                       ' from the year and the month specified.Default 0.')
 
     args = parser.parse_args()
     return args
