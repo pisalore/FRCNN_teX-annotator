a05b45800b52f9d910424f4692fc263e3b857b6b
pisalore <lorenzo.pisaneschi@gmail.com>
2020-03-15 19:47:28 +0100

    Modified test images paths for test_frcnn.py

--- venv/frcnn/test_frcnn.py
+++ venv/frcnn/test_frcnn.py
@@ -146,102 +146,106 @@ classes = {}
 bbox_threshold = 0.8
 
 visualise = True
+#TODO: iterate over 'annotated_images' dirs, then images
 
-for idx, img_name in enumerate(sorted(os.listdir(img_path))):
-	if not img_name.lower().endswith(('.bmp', '.jpeg', '.jpg', '.png', '.tif', '.tiff')):
-		continue
-	print(img_name)
-	st = time.time()
-	filepath = os.path.join(img_path,img_name)
+list_subfolders = [f.path for f in os.scandir(img_path) if f.is_dir()]
+print(list_subfolders)
+for subdir in list_subfolders:
+	for idx, img_name in enumerate(sorted(os.listdir(subdir))):
+		if not img_name.lower().endswith(('.bmp', '.jpeg', '.jpg', '.png', '.tif', '.tiff')):
+			continue
+		print(img_name)
+		st = time.time()
+		filepath = os.path.join(subdir, img_name)
 
-	img = cv2.imread(filepath)
+		img = cv2.imread(filepath)
 
-	X, ratio = format_img(img, C)
+		X, ratio = format_img(img, C)
 
-	if K.image_dim_ordering() == 'tf':
-		X = np.transpose(X, (0, 2, 3, 1))
+		if K.image_dim_ordering() == 'tf':
+			X = np.transpose(X, (0, 2, 3, 1))
 
-	# get the feature maps and output from the RPN
-	[Y1, Y2, F] = model_rpn.predict(X)
-	
+		# get the feature maps and output from the RPN
+		[Y1, Y2, F] = model_rpn.predict(X)
 
-	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.7)
 
-	# convert from (x1,y1,x2,y2) to (x,y,w,h)
-	R[:, 2] -= R[:, 0]
-	R[:, 3] -= R[:, 1]
+		R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.7)
 
-	# apply the spatial pyramid pooling to the proposed regions
-	bboxes = {}
-	probs = {}
+		# convert from (x1,y1,x2,y2) to (x,y,w,h)
+		R[:, 2] -= R[:, 0]
+		R[:, 3] -= R[:, 1]
 
-	for jk in range(R.shape[0]//C.num_rois + 1):
-		ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
-		if ROIs.shape[1] == 0:
-			break
+		# apply the spatial pyramid pooling to the proposed regions
+		bboxes = {}
+		probs = {}
 
-		if jk == R.shape[0]//C.num_rois:
-			#pad R
-			curr_shape = ROIs.shape
-			target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
-			ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
-			ROIs_padded[:, :curr_shape[1], :] = ROIs
-			ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
-			ROIs = ROIs_padded
+		for jk in range(R.shape[0]//C.num_rois + 1):
+			ROIs = np.expand_dims(R[C.num_rois*jk:C.num_rois*(jk+1), :], axis=0)
+			if ROIs.shape[1] == 0:
+				break
 
-		[P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
+			if jk == R.shape[0]//C.num_rois:
+				#pad R
+				curr_shape = ROIs.shape
+				target_shape = (curr_shape[0],C.num_rois,curr_shape[2])
+				ROIs_padded = np.zeros(target_shape).astype(ROIs.dtype)
+				ROIs_padded[:, :curr_shape[1], :] = ROIs
+				ROIs_padded[0, curr_shape[1]:, :] = ROIs[0, 0, :]
+				ROIs = ROIs_padded
 
-		for ii in range(P_cls.shape[1]):
+			[P_cls, P_regr] = model_classifier_only.predict([F, ROIs])
 
-			if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
-				continue
+			for ii in range(P_cls.shape[1]):
 
-			cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
+				if np.max(P_cls[0, ii, :]) < bbox_threshold or np.argmax(P_cls[0, ii, :]) == (P_cls.shape[2] - 1):
+					continue
 
-			if cls_name not in bboxes:
-				bboxes[cls_name] = []
-				probs[cls_name] = []
+				cls_name = class_mapping[np.argmax(P_cls[0, ii, :])]
 
-			(x, y, w, h) = ROIs[0, ii, :]
+				if cls_name not in bboxes:
+					bboxes[cls_name] = []
+					probs[cls_name] = []
 
-			cls_num = np.argmax(P_cls[0, ii, :])
-			try:
-				(tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
-				tx /= C.classifier_regr_std[0]
-				ty /= C.classifier_regr_std[1]
-				tw /= C.classifier_regr_std[2]
-				th /= C.classifier_regr_std[3]
-				x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
-			except:
-				pass
-			bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
-			probs[cls_name].append(np.max(P_cls[0, ii, :]))
+				(x, y, w, h) = ROIs[0, ii, :]
 
-	all_dets = []
+				cls_num = np.argmax(P_cls[0, ii, :])
+				try:
+					(tx, ty, tw, th) = P_regr[0, ii, 4*cls_num:4*(cls_num+1)]
+					tx /= C.classifier_regr_std[0]
+					ty /= C.classifier_regr_std[1]
+					tw /= C.classifier_regr_std[2]
+					th /= C.classifier_regr_std[3]
+					x, y, w, h = roi_helpers.apply_regr(x, y, w, h, tx, ty, tw, th)
+				except:
+					pass
+				bboxes[cls_name].append([C.rpn_stride*x, C.rpn_stride*y, C.rpn_stride*(x+w), C.rpn_stride*(y+h)])
+				probs[cls_name].append(np.max(P_cls[0, ii, :]))
 
-	for key in bboxes:
-		bbox = np.array(bboxes[key])
+		all_dets = []
 
-		new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
-		for jk in range(new_boxes.shape[0]):
-			(x1, y1, x2, y2) = new_boxes[jk,:]
+		for key in bboxes:
+			bbox = np.array(bboxes[key])
 
-			(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
+			new_boxes, new_probs = roi_helpers.non_max_suppression_fast(bbox, np.array(probs[key]), overlap_thresh=0.5)
+			for jk in range(new_boxes.shape[0]):
+				(x1, y1, x2, y2) = new_boxes[jk,:]
 
-			cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
+				(real_x1, real_y1, real_x2, real_y2) = get_real_coordinates(ratio, x1, y1, x2, y2)
 
-			textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
-			all_dets.append((key,100*new_probs[jk]))
+				cv2.rectangle(img,(real_x1, real_y1), (real_x2, real_y2), (int(class_to_color[key][0]), int(class_to_color[key][1]), int(class_to_color[key][2])),2)
 
-			(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
-			textOrg = (real_x1, real_y1-0)
+				textLabel = '{}: {}'.format(key,int(100*new_probs[jk]))
+				all_dets.append((key,100*new_probs[jk]))
 
-			cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
-			cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
-			cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+				(retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,1,1)
+				textOrg = (real_x1, real_y1-0)
 
-	print('Elapsed time = {}'.format(time.time() - st))
-	print(all_dets)
-	# cv2.imshow('img', img)
-	# cv2.waitKey(0)
-	cv2.imwrite('./results_imgs/{}.png'.format(idx),img)
+				cv2.rectangle(img, (textOrg[0] - 5, textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (0, 0, 0), 2)
+				cv2.rectangle(img, (textOrg[0] - 5,textOrg[1]+baseLine - 5), (textOrg[0]+retval[0] + 5, textOrg[1]-retval[1] - 5), (255, 255, 255), -1)
+				cv2.putText(img, textLabel, textOrg, cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 0), 1)
+
+		print('Elapsed time = {}'.format(time.time() - st))
+		print(all_dets)
+		# cv2.imshow('img', img)
+		# cv2.waitKey(0)
+		cv2.imwrite('./results_imgs/{}.png'.format(idx),img)
