7a1d0e67ab7130c723c181f51e4395d4dfaccf11
pisalore <lorenzo.pisaneschi@gmail.com>
2020-03-22 21:14:17 +0100

    file processed counter

--- venv/test_images_annotator.py
+++ venv/test_images_annotator.py
@@ -9,13 +9,16 @@ tex_files = './tex_files/'
 csv_file_path = 'test_images_annotations.csv'
 txt_path = 'frcnn/annotated_test_images.txt'
 list_subfolders = [f.path for f in os.scandir(test_imgs_path) if f.is_dir()]
+file_processed = 0
 for subdir in list_subfolders:
     file_id = os.path.basename(subdir).split('_annotated_images')[0]
     print(file_id)
     pdf_file_path = pdf_files + os.path.basename(subdir).split('_annotated_images')[0] + '.pdf'
     tex_file_path = tex_files + file_id + '_tex_files'
     detected_objects = parse_pdf(pdf_file_path, tex_file_path, 'no', True, False)
+    file_processed += 1
     generate_csv_annotations(csv_file_path, file_id, detected_objects)
+    print(file_processed)
 
 obtain_txt_train_images_file(csv_file_path, txt_path, '..png_files/test_images/')
 
