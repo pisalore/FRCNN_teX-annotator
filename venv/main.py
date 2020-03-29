a388069b3e03b41893d46234652c404e463ee40c
pisalore <lorenzo.pisaneschi@gmail.com>
2020-03-29 16:33:18 +0200

    refactoring

--- venv/main.py
+++ venv/main.py
@@ -10,6 +10,7 @@ PDF_FILES = 'pdf_files/'
 TEX_FILES = 'tex_files/'
 PNG_FILES_TRAIN = 'png_files/train_images/'
 
+
 def main():
     args = main_args()
     is_annotation = args.annotations
@@ -17,7 +18,7 @@ def main():
     num_files = len([f for f in os.listdir(PDF_FILES) if os.path.isfile(os.path.join(PDF_FILES, f))])
     num_train_images = num_files / 100 * 90
     files_processed = 0
-    if (os.path.exists(csv_file_path)):
+    if os.path.exists(csv_file_path):
         os.remove(csv_file_path)
     for pdf_file_path in glob.glob(os.path.join(PDF_FILES, '*.pdf')):
         if files_processed < num_train_images:
@@ -27,28 +28,29 @@ def main():
             is_train = False
         file_id = str(os.path.basename(pdf_file_path).split('.pdf')[0])
         tex_file_path = TEX_FILES + file_id + '_tex_files'
-        if(os.path.exists(tex_file_path)):
-            print('\nParsing ' + pdf_file_path +'...')
+        if os.path.exists(tex_file_path):
+            print('\nParsing ' + pdf_file_path + '...')
             try:
                 detected_objects = parse_pdf(pdf_file_path, tex_file_path, is_annotation, is_train, True)
             except:
                 detected_objects = []
                 if os.path.exists(PNG_FILES_TRAIN + file_id + '_annotated_images'):
                     shutil.rmtree(PNG_FILES_TRAIN + file_id + '_annotated_images')
-            if(detected_objects and is_train):
+            if detected_objects and is_train:
                 print('Save annotations...')
                 generate_csv_annotations(csv_file_path, file_id, detected_objects)
             elif not is_train:
-                print ('Added test images.')
+                print('Added test images.')
 
             elif not detected_objects:
-                print ('ERROR IN PARSING FILES, GO ON.')
+                print('ERROR IN PARSING FILES, GO ON.')
 
             print('Num paper processed: ', files_processed)
 
     if os.path.exists(csv_file_path):
         txt_path = 'frcnn/annotated_train_images.txt'
         obtain_txt_train_images_file(csv_file_path, txt_path, '..png_files/train_images/')
+
 
 if __name__ == "__main__":
     main()
--- venv/utils.py
+++ venv/utils.py
@@ -1,25 +1,33 @@
 import argparse
 
+
 def download_script_parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--year', default='20', help=' Choose the year from which you want to start'
-                                                       ' downloading papers from arXIv. Default 20.')
-    parser.add_argument('--month', default='1', help=' Choose the month, once you have choseh the year, from which you want to start'
-                                                     ' downloading papers from arXIv. Default 1(janaury)')
-    parser.add_argument('--counter', default='0', help='Choose the starting file counter. With 0 you will download all files'
-                                                       ' from the year and the month specified.Default 0.')
+    parser.add_argument('--year', default='20',
+                        help=' Choose the year from which you want to start'
+                             ' downloading papers from arXIv. Default 20.')
+    parser.add_argument('--month', default='1',
+                        help=' Choose the month, once you have choseh the year, from which you want to start'
+                             ' downloading papers from arXIv. Default 1(janaury)')
+    parser.add_argument('--counter', default='0',
+                        help='Choose the starting file counter. With 0 you will download all files'
+                             ' from the year and the month specified.Default 0.')
     parser.add_argument('--max_items', default='1', help='Chose how many files you will download')
 
     args = parser.parse_args()
     return args
 
+
 def main_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_file_path', default='images_annotations.csv',
                         help='Type the .csv file name to convert. By default is: images_annotations. '
-                             'The converter then will generate test_annotations_images.txt and train_annotations_images.txt .')
-    parser.add_argument('--annotations', default='no', help='Choose if generate annotated images where:'
-                                                            ' red= titles; green= figures; blu= lists; aqua green= tables; yellow= text;'
-                                                            ' typing yes or no.')
+                             'The converter then will generate test_annotations_images.txt and train_'
+                             'annotations_images.txt .')
+    parser.add_argument('--annotations', default='no',
+                        help='Choose if generate annotated images where:'
+                             ' red= titles; green= figures; blu= lists; aqua green= tables; yellow= text;'
+                             ' typing yes or no.')
     args = parser.parse_args()
     return args
+
