5eb0dbe4fb0c7791dedcc2ca9b2f48cce2e4ef8e
pisalore <lorenzo.pisaneschi@gmail.com>
2020-03-30 19:11:35 +0200

    managing papers evaluation when pred and gt pages are not the same (ex: gt has a page which pred does not have and/or viceversa)

--- venv/frcnn/intersection_over_union/intersection_over_union.py
+++ venv/frcnn/intersection_over_union/intersection_over_union.py
@@ -1,13 +1,25 @@
 import numpy as np
 
 
+# iterate over any papers couple (gt, pred), calculates the iou, precision and recall
 def evaluate_test_results(gt_papers_list, pred_papers_list):
     for gt, pred in zip(gt_papers_list, pred_papers_list):
         process_gt_and_pred_papers(gt, pred)
 
 
-def process_gt_and_pred_papers(gt_paper, pred_paer):
-    print('Processing papers... gt: ', gt_paper.paper_name, 'pred: ', pred_paer.paper_name)
+def process_gt_and_pred_papers(gt_paper, pred_paper):
+    print('Processing papers... gt: ', gt_paper.paper_name, 'pred: ', pred_paper.paper_name)
+    paper_analytics = PaperAnalytics
+    gt_paper_pages = gt_paper.pages
+    pred_paper_pages = pred_paper.pages
+    paper_analytics.analyzed_paper_name = gt_paper.paper_name
+    for i, j in zip(range(len(gt_paper_pages)), range(len(pred_paper_pages))):
+        if gt_paper_pages[i].page_number == pred_paper_pages[j].page_number:
+            print('ok, evaluate')
+        elif gt_paper_pages[i].page_number > pred_paper_pages[j].page_number:
+            print('all pred page instances are false positives, iou is 0 for all of them')
+        else:
+            print('all gt page instances are false negatives, iou is 0 for all of them')
 
 
 def compute_iou(gt, pred):
@@ -16,6 +28,7 @@ def compute_iou(gt, pred):
 
 class PaperAnalytics:
     def __init__(self):
+        self.analyzed_paper_name = None
         self.pages_analyzes = None
         self.overall_precision = None
         self.overall_recall = None
