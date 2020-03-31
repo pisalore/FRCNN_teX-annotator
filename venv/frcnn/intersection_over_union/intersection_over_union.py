94f0ef76e73511ec75fc641f3b7fbd4a04bd7876
pisalore <lorenzo.pisaneschi@gmail.com>
2020-03-31 20:03:10 +0200

    minor refactoring

--- venv/frcnn/intersection_over_union/intersection_over_union.py
+++ venv/frcnn/intersection_over_union/intersection_over_union.py
@@ -9,21 +9,27 @@ log_path = './logs/test_results.txt'
 
 # iterate over any papers couple (gt, pred), calculates the iou, precision and recall
 def evaluate_test_results(gt_papers_list, pred_papers_list):
+    # In pages_analyzes all paper analytics will be saved
+    papers_analyzes = []
     if os.path.exists(log_path):
         os.remove(log_path)
     results_test_log_file = open(log_path, 'a+')
     results_test_log_file.write('TEST RESULTS LOG \n')
     results_test_log_file.close()
     for gt, pred in zip(gt_papers_list, pred_papers_list):
-        process_gt_and_pred_papers(gt, pred)
+        paper_analytics = process_gt_and_pred_papers(gt, pred)
+        papers_analyzes.append(paper_analytics)
+
+    return papers_analyzes
 
 
 def process_gt_and_pred_papers(gt_paper, pred_paper):
     print('Processing papers... gt: ', gt_paper.paper_name, 'pred: ', pred_paper.paper_name)
+    # In pages_analyzes all examined paper pages analytics will be saved
+    pages_analyzes = []
     results_test_log_file = open(log_path, 'a+')
     results_test_log_file.write('ANALYZED PAPER: ' + gt_paper.paper_name + '\n')
     results_test_log_file.close()
-    analyzed_paper_pages = []
     # Loading pages matched between gt and pred papers and different pages
     matched_pages, additional_gt_pages, additional_pred_pages = verify_paper_pages_correspondences(gt_paper, pred_paper)
     # instantiate the paper analytics obj where save all the information collected for a paper in test analysis
@@ -39,12 +45,18 @@ def process_gt_and_pred_papers(gt_paper, pred_paper):
     for page in matched_pages:
         matched_gt_page = next(gt_page for gt_page in gt_paper.pages if gt_page.page_number == page)
         matched_pred_page = next(pred_page for pred_page in pred_paper.pages if pred_page.page_number == page)
+        # here the page analysis is computed
         page_analytics = process_page_analysis(matched_gt_page, matched_pred_page)
-        analyzed_paper_pages.append(page_analytics)
+        pages_analyzes.append(page_analytics)
+
+    paper_analytics.pages_analyzes = pages_analyzes
+    return paper_analytics
 
 
 def process_page_analysis(gt_page, pred_page):
+    # Page-> page_instances
     page_analytics = PageAnalytics()
+
     return page_analytics
 
 
